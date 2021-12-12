fc575f5602727993b7cbbf17a31c9dfc84fbf91e
Trevor Malesi <93514742+Malesi-Trevor-Git@users.noreply.github.com>
2021-12-13 00:31:52 +0300

    Failed to scrape the data

--- jumia-scraper.py
+++ jumia-scraper.py
@@ -1,22 +1,57 @@
-import os
-import pandas as pd
-import pandas as pd
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from bs4 import BeautifulSoup
 import requests
-from bs4 import BeautifulSoup as beautiful
-url="https://www.jumia.co.ke/android-phone/"
-all_urls=[]
-for page in range(10):
-    next_urls=url+'?page='+str(page)
-    all_urls.append(next_urls)
-for url in all_urls:
-    render=requests.get(url)
-    the_html=beautiful(render.content,'html.parser')
-    scrape=the_html.find_all(class_='text-area')
-    scraped_data=[]
-    for data in scrape:
-        scraped_data.append(data.get_text())
-        clean_data=[data.replace('\n','') for data in scraped_data]
-        clean_data_=[data.replace(' ','') for data in clean_data]
-        data_2_csv=pd.DataFrame(clean_data_,columns=['column'])
-        data_2_csv.to_csv('jumia-deals',index=False,mode="a")
-        print(data_2_csv)
+import csv
+
+seller = 'la-colors'
+pagination = '3'
+siteUrl = 'https://www.jumia.co.ke/'
+headers = {'User-Agent': 'Mozilla/5.0'}
+page= '?page='+pagination
+
+baseUrl = siteUrl+seller+page
+#fullUrl = 
+#page = urllib2.urlopen(baseUrl)
+# Here, we're just importing both Beautiful Soup and the Requests library
+# this is the url that we've already determined is safe and legal to scrape from.
+page_response = requests.get(baseUrl, timeout=5)
+# here, we fetch the content from the url, using the requests library
+soup = BeautifulSoup(page_response.content, "html.parser")
+#we use the html parser to parse the url content and store it in a variable.
+section = soup.find('section', attrs={'class':'products'})
+
+dataToCsv = []
+for productContainer in section:
+    try:
+       productName = productContainer.find("span", attrs={'class':'name'}).text
+       price = productContainer.find("span", attrs={'class':'price'}).text
+       reviews = productContainer.find("div", attrs={'class':'total-ratings'}).text 
+       brand = productContainer.find("span", attrs={'class':'brand'}).text 
+    except Exception as e:
+      productName = "N"
+      price = "N"
+      reviews = "N"
+      brand = "N"
+      
+    if productName != 'N':
+#        print(hotelDesc)
+#        print(productName)
+#        print(price) #remove ksh
+        
+#        print(brand) #remove ksh
+#        print(reviews) #remove brackets
+        dataToCsv.append([productName,brand.replace('\xa0', ''),price[4:],reviews.strip("()")])
+        #empty line
+#        print("==============================")
+#print(dataToCsv)
+
+with open(seller+'_'+pagination+'.csv', 'w', newline='') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(dataToCsv)
+
+csvFile.close()
